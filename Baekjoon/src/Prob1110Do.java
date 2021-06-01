e3d1be8367333760b6ce3dfe4b5942206db4668c
ekank <ekankr2@naver.com>
2021-06-01 10:32:42 +0900

    [ImIkHwan]BaekJoon1110 while

--- Baekjoon/src/Prob1110Do.java
+++ Baekjoon/src/Prob1110Do.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
-public class Prob1110 {
+public class Prob1110Do {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-            int N = scan.nextInt();
-            int M = N;
-            int i = 0;
-        while (true){
+        int N = scan.nextInt();
+        int M = N;
+        int i = 0;
+        do{
             N = (N % 10 * 10) + ((N / 10 + N % 10)%10);
             i++;
-            if(N==M){
-                break;
-            }
-        }
+        }while (N!=M);
         System.out.println(i);
-
     }
 }
