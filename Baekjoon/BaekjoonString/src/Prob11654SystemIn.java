a1913f87200db71d162356dfbfe2322952a02978
ekank <ekankr2@naver.com>
2021-06-17 07:35:20 +0900

    prob11654 using System.in.read();

--- Baekjoon/BaekjoonString/src/Prob11654SystemIn.java
+++ Baekjoon/BaekjoonString/src/Prob11654SystemIn.java
@@ -1,2 +1,8 @@
 public class Prob11654SystemIn {
-}
+        public static void main(String[] args) throws Exception {
+
+            int a = System.in.read();
+            System.out.print(a);
+
+        }
+    }
