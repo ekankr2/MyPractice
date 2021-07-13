d7605e20e0323fe9640890bc433a4a9204fc99bb
ekank <ekankr2@naver.com>
2021-07-14 02:43:21 +0900

    vudongsan : v-for on data

--- Vue/vudongsan/src/App.vue
+++ Vue/vudongsan/src/App.vue
@@ -1,44 +1,26 @@
 <template>
 
-  <div class="black-bg" v-if="모달창열렸니 == true">
-    <div class="white-bg">
-      <span v-on:click="close">닫기</span>
-      <h4>상세페이지임</h4>
-      <p>상세페이지 내용임</p>
-    </div>
-  </div>
   <div class="menus">
     <a v-for="(menu, index) in 메뉴들" :key="index ">{{ menu }}</a>
   </div>
 
-  <div>
-    <img src="./assets/room0.jpg" class="room-img">
-    <h4 class="productName" @click="모달창열렸니 = true">{{  products[0] }}</h4>
-    <p>50 만원</p>
-    <button v-on:click="increase(0)">허위매물신고</button><span>신고수 : {{신고수[0]}}</span>
-  </div>
-  <div>
-    <img src="./assets/room1.jpg" class="room-img">
-    <h4 @click="모달창열렸니 = true">{{ products[1] }}</h4>
-    <p>70 만원</p>
-    <button v-on:click="increase(1)">허위매물신고</button><span>신고수 : {{신고수[1]}}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.jpg" class="room-img">
-    <h4 @click="모달창열렸니 = true">{{ products[2] }}</h4>
-    <p>80 만원</p>
-    <button v-on:click="increase(2)">허위매물신고</button><span>신고수 : {{신고수[2]}}</span>
+  <div v-for="(items, index) in 원룸들" :key="index">
+    <img :src="items.image" class="room-img">
+    <h4>{{  items.title }}</h4>
+    <p>{{  items.price }}원</p>
   </div>
 </template>
 
 <script>
 
+import rooms from './assets/oneroom';
 
 
 export default {
   name: 'App',
   data(){
     return {
+      원룸들 : rooms,
       모달창열렸니 : false,
       신고수 : [0,0,0],
       메뉴들 : ['Home','Shop','About'],
