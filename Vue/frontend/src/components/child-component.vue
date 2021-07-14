ac2d1b9c161c4a6cf6f1508fbfbd027956ca0a51
ekank <ekankr2@naver.com>
2021-07-15 06:38:55 +0900

    understanding vue

---
+++ Vue/frontend/src/components/child-component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="sendEvent()">Print</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'child-component',
+  props: {
+    parentMessage: {
+      type: String,
+      default: void 0
+    }
+  },
+  methods: {
+    sendEvent () {
+      this.$emit('print', this.parentMessage)
+    }
+  }
+}
+</script>
