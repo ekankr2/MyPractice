d24322d0b0258f3a17043b5cfd62b3b0c9435fa2
ekank <ekankr2@naver.com>
2021-09-22 23:54:03 +0900

    react useState

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,19 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
 
-    let posts = '강남 고기 맛집';
-    function 함수() {
-        return 100
-    }
+    let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
 
+    let posts = '강남 고기 맛집'
   return (
     <div className="App">
       <div className="black-nav">
-          <div style={ { color : 'blue', fontSize : '30px' } }>개발 Blog</div>
+          <div>개발 Blog</div>
       </div>
-        <h4> { posts }</h4>
+        <div className="list">
+            <h3> { 글제목[1] }</h3>
+            <p>2월 17일 발행</p>
+            <hr/>
+        </div>
     </div>
   );
 }
