862aa3d7b65daab03c5c6de5eed8badff40e5bed
ekank <ekankr2@naver.com>
2021-10-05 21:25:20 +0900

    React props

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -61,7 +61,7 @@ function App() {
 
         {
             modal === true
-            ? <Modal></Modal>
+            ? <Modal 글제목={글제목}></Modal>
                 : null
         }
 
@@ -69,10 +69,10 @@ function App() {
   );
 }
 
-function Modal() {
+function Modal(props) {
     return(
         <div className="modal">
-            <h2>제목</h2>
+            <h2>제목{ props.글제목[0] }</h2>
             <p>날짜</p>
             <p>상세내용</p>
         </div>
