617bf5a9824efb8537c75a23ea4307790652aec0
ekank <ekankr2@naver.com>
2021-11-25 22:12:37 +0900

    typescript practice

--- React/superchat/src/App.js
+++ React/superchat/src/App.js
@@ -1,7 +1,7 @@
 import React, {useRef, useState} from "react";
 import './App.css';
 
-import firebase from "firebase/compat";
+import firebase  from "firebase/compat";
 import 'firebase/firestore'
 import 'firebase/auth'
 
---
+++ TypeScript/index.ts
@@ -0,0 +1,19 @@
+let 이름 = 'kim';
+let 나이 :number = 50
+let 결혼했니 :boolean = true;
+
+let 회원들 :string[] = ['kim', 'park']
+
+let 사람들 :{ member1 : string, member2 : string } = { member1 : 'kim', member2 : 'park'}
+
+let myname :string = 'ekan';
+let age :number = 26;
+let from :string = 'korea'
+
+let 노래 :{ song : string, name : string} = { song : 'Home', name : 'park'}
+
+let project :{ member : string[], days : number, started : boolean }= {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
+}
