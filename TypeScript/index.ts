163a847ecbf417c5d16416ec7a8918768709caa3
ekank <ekankr2@naver.com>
2021-11-26 13:37:01 +0900

    any and unknown

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -17,3 +17,20 @@ let project :{ member : string[], days : number, started : boolean }= {
     days : 30,
     started : true,
 }
+// Union Type
+let 회원 :number | string = 123;
+
+let 회원들2 :(number | string)[] = [1, '2', 3]
+let 오브젝트 :{ a: string | number } = { a : '123'}
+
+// 타입쉴드 해제 문법
+let 이름애니 :any;
+이름애니 = 123;
+이름애니 = '123';
+
+let 이름어농 :unknown;
+이름어농 = 123;
+이름어농 = {};
+
+// let 변수1 :string = 이름어농  ===> error
+let 변수1 :string = 이름애니 // no error
