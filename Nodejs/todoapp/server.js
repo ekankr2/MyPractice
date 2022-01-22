878af28ef4be2eb484ff3861546c4f5388b50020
ekank <ekankr2@naver.com>
2022-01-22 20:29:07 +0900

    nodemon

--- Nodejs/todoapp/server.js
+++ Nodejs/todoapp/server.js
@@ -9,3 +9,7 @@ app.listen(8080, function (){
 app.get('/pet', function (요청, 응답){
     응답.send('펫용품 쇼핑할 수 있는 페이지입니다.')
 })
+
+app.get('/beauty', function (요청, 응답){
+    응답.send('뷰티용품 쇼핑할 수 있는 페이지입니다.')
+})
