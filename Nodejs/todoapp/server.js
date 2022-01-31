0315f9ed8de3e5f6e5a6e24c7d0c34e0838e1821
ekank <ekankr2@naver.com>
2022-01-31 17:43:02 +0900

    post

--- Nodejs/todoapp/server.js
+++ Nodejs/todoapp/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(8080, function (){
     console.log('listening on 8080')
@@ -17,3 +19,13 @@ app.get('/beauty', function (요청, 응답){
 app.get('/', function (요청, 응답){
     응답.sendFile(__dirname + '/index.html')
 })
+
+app.get('/write', function (요청, 응답){
+    응답.sendFile(__dirname + '/write.html')
+})
+
+app.post('/add', function (요청, 응답){
+    응답.send('전송완료')
+    console.log(요청.body.title)
+})
+
