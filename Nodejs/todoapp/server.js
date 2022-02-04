3303c28be21edf4109b20cfd742987b3502e9e30
ekank <ekankr2@naver.com>
2022-02-04 20:57:08 +0900

    ejs

--- Nodejs/todoapp/server.js
+++ Nodejs/todoapp/server.js
@@ -3,23 +3,19 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 const MongoClient = require('mongodb').MongoClient
+app.set('view engine', 'ejs')
 
 var db;
+
+app.listen(8080, function (){
+    console.log('listening on 8080')
+})
+
 MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.7v3za.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function (에러, client){
 
     if(에러) return console.log(에러)
     db = client.db('todoapp')
-
-    db.collection('post').insertOne({이름: 'John', _id: 100}, function (에러, 결과){
-        console.log('저장완료')
     })
-
-    app.listen(8080, function (){
-        console.log('listening on 8080')
-    })
-
-})
-
 
 
 
@@ -41,6 +37,15 @@ app.get('/write', function (요청, 응답){
 
 app.post('/add', function (요청, 응답){
     응답.send('전송완료')
+    console.log(요청.body.date)
     console.log(요청.body.title)
+
+    db.collection('post').insertOne({제목: 요청.body.title, 날짜: 요청.body.date}, function (에러, 결과){
+        console.log('저장완료')
+    })
 })
 
+
+app.get('/list', function (요청, 응답){
+    응답.render('list.ejs')
+})
