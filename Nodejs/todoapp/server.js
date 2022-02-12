1a2177c631b37cda38f5539d09a7bad1b46e84d7
ekank <ekankr2@naver.com>
2022-02-12 20:57:32 +0900

    error 처리

--- Nodejs/todoapp/server.js
+++ Nodejs/todoapp/server.js
@@ -68,5 +68,6 @@ app.delete('/delete', function (요청, 응답){
 
     db.collection('post').deleteOne(요청.body, function (에러, 결과){
         console.log('삭제완료')
+        응답.status(200).send({ message : '성공했습니다' })
     })
 })
