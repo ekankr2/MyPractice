ab244f37c52904ec8ad52fbb04b4bbf78ea4afe3
ekank <ekankr2@naver.com>
2022-02-19 21:46:20 +0900

    method-override

--- Nodejs/todoapp/server.js
+++ Nodejs/todoapp/server.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 const MongoClient = require('mongodb').MongoClient
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 
 app.use('/public', express.static('public'))
@@ -29,11 +31,11 @@ app.get('/beauty', function (요청, 응답) {
 })
 
 app.get('/', function (요청, 응답) {
-    응답.sendFile(__dirname + '/index.html')
+    응답.render('index.ejs')
 })
 
 app.get('/write', function (요청, 응답) {
-    응답.sendFile(__dirname + '/write.html')
+    응답.render('write.ejs')
 })
 
 app.post('/add', function (요청, 응답) {
@@ -81,5 +83,11 @@ app.get('/detail/:id', function (요청, 응답) {
         console.log(결과)
         응답.render('detail.ejs', {data: 결과})
     })
+})
 
+app.get('/edit/:id', function (요청, 응답){
+
+    db.collection('post').findOne({_id : parseInt(요청.params.id)}, function (에러, 결과){
+        응답.render('edit.ejs', {post : 결과})
+    })
 })
