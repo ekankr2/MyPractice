51144be47c4a85972c09f4a4238665026c7b8d3b
ekank <ekankr2@naver.com>
2022-03-03 23:02:09 +0900

    search function

--- Nodejs/todoapp/server.js
+++ Nodejs/todoapp/server.js
@@ -58,6 +58,13 @@ app.post('/add', function (요청, 응답) {
     })
 })
 
+app.get('/search', (요청, 응답) => {
+    console.log(요청.query.value)
+    db.collection('post').find({제목: 요청.query.value}).toArray((에러, 결과) => {
+        console.log(결과)
+    })
+})
+
 
 app.get('/list', function (요청, 응답) {
 
