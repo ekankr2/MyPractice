d3e049cb70e7f1a4523d3cf77897fb227e493b35
ekank <ekankr2@naver.com>
2022-04-02 21:56:24 +0900

    realtime db

--- Nodejs/todoapp/server.js
+++ Nodejs/todoapp/server.js
@@ -272,3 +272,21 @@ app.post('/message', 로그인했니, function (요청, 응답){
         응답.send('DB저장성공')
     })
 })
+
+app.get('/message/:id', 로그인했니, function (요청, 응답){
+    응답.writeHead(200, {
+        "Connection" : "keep-alive",
+        "Content-Type" : "text/event-stream",
+        "Cache-Control": "no-cache",
+    })
+
+    db.collection('message').find({ parent : 요청.params.id }).toArray()
+        .then((결과) => {
+            응답.write('event: test\n');
+            응답.write('data:' + JSON.stringify(결과) + '\n\n')
+        })
+
+    응답.write('event: test\n');
+    응답.write('data: 안녕하세요\n\n');
+
+})
