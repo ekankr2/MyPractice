ac8a8e36f70255d21d585c63b9af057bc26cce84
ekankr2 <ekankr2@naver.com>
2022-07-07 20:17:40 +0900

    get set

--- JavaScript/es6/getter setter.js
+++ JavaScript/es6/getter setter.js
@@ -1,34 +1,30 @@
-class 사람 {
-    constructor() {
-        this.name = 'Park'
-        this.age = 20
-    }
-    get nextAge() {
-        return this.age + 1
-    }
-    set setAge(나이) {
-        this.age = 나이
-    }
-}
-
-var 사람1 = new 사람()
-사람1.setAge = 200
-console.log(사람1)
-
-
-// var 사람 = {
-//     name : 'Park',
-//     age : 30,
-//     // return이 있어야한다.
+// class 사람 {
+//     constructor() {
+//         this.name = 'Park'
+//         this.age = 20
+//     }
 //     get nextAge() {
 //         return this.age + 1
-//     },
-//     // 파라미터 입력 필수
-//     set setAge(age) {
-//         this.age = parseInt(age)
+//     }
+//     set setAge(나이) {
+//         this.age = 나이
 //     }
 // }
 //
-// 사람.setAge = '200'
-//
-// console.log(사람.nextAge)
+// var 사람1 = new 사람()
+// 사람1.setAge = 200
+// console.log(사람1)
+
+
+var 사람 = {
+    name : 'Park',
+    age : 30,
+    get nextAge(){
+        return this.age + 1
+    },
+    set setAge(age){
+        this.age = age
+    }
+}
+
+console.log(사람.nextAge)
