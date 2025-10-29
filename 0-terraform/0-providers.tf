f1ce3563f5d26ed87043fcfe99296e7b6e1648ac
IkHwan Im <ekankr2@naver.com>
2025-10-29 16:56:48 +0900

    init terraform

---
+++ 0-terraform/0-providers.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  profile = "devopsboy"
+  region  = "ap-northeast-2"
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
